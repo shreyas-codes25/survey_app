29ce2298b16ad680755123e91b60f0add35c4ba9
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-06-06 14:13:06 +0530

    question.dart file which holds all the questions which are intended to be shown

---
+++ lib/answer_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      { required this.buttonText,
+        required this.onTap,
+        super.key});
+  final void Function() onTap;
+  final String buttonText;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(200, 40),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        shape: const StadiumBorder(
+          side: BorderSide(
+            color: Colors.white,
+            width: 2,
+          ),
+        ),
+      ),
+      child: Text(buttonText),
+      onPressed: () {},
+    );
+  }
+}
--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:survey_app/answer_button.dart';
+import 'package:survey_app/data/questions.dart';
 
 class Questions extends StatefulWidget {
   const Questions({super.key});
@@ -10,8 +12,34 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Questions Screen"),
+    final currQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Text(
+              currQuestion.question,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                fontWeight:FontWeight.bold,
+
+              ),
+            ),
+             SizedBox(
+              height: 30,
+            ),
+            AnswerButton(buttonText: currQuestion.answers[0], onTap: (){}),
+            AnswerButton(buttonText: currQuestion.answers[1], onTap: (){}),
+            AnswerButton(buttonText: currQuestion.answers[2], onTap: (){}),
+            AnswerButton(buttonText: currQuestion.answers[3], onTap: (){}),
+
+
+
+
+          ]),
     );
   }
 }
